5326646ab4bdf23bb29916b4a491c295702baff1
codeandcodeandcode <codepkcode49@gmail.com>
2022-06-17 13:18:56 +0300

    commit tree 2cda679649b8f518af78047103961d2b28b6db48

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Add your code here
+fetch(destinationURL, configurationObject);
+
+const formData = {
+    dogName: "Byron",
+    dogBreed: "Poodle",
+  };
+
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+  
+  fetch("http://localhost:3000/dogs", configurationObject)
+
